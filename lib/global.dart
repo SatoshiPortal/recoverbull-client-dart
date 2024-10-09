dec0d6940e3bf608fb449e381d11f494a4ec98c2
ethicnology <ethicnology@pm.me>
2024-10-09 09:15:58 -0400

    feat: homepage to edit/show private key

---
+++ lib/global.dart
@@ -0,0 +1,7 @@
+var _privateKey =
+    'c37c299bb7f5ffd8d9329d052983342a8c3234ff3b3fa32a292187341f7146d7';
+
+class Global {
+  static String get privateKey => _privateKey;
+  static set privateKey(String v) => _privateKey = v;
+}
