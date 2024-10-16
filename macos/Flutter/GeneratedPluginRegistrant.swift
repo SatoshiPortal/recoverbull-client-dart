f1b594d42bf39d79deb005b9b03918430c930e76
ethicnology <ethicnology@pm.me>
2024-10-15 20:39:12 -0400

    chore: dependencies

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import fast_rsa
 import file_saver
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FastRsaPlugin.register(with: registry.registrar(forPlugin: "FastRsaPlugin"))
   FileSaverPlugin.register(with: registry.registrar(forPlugin: "FileSaverPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
--- windows/flutter/generated_plugin_registrant.cc
+++ windows/flutter/generated_plugin_registrant.cc
@@ -6,9 +6,12 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <fast_rsa/fast_rsa_plugin.h>
 #include <file_saver/file_saver_plugin.h>
 
 void RegisterPlugins(flutter::PluginRegistry* registry) {
+  FastRsaPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("FastRsaPlugin"));
   FileSaverPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("FileSaverPlugin"));
 }
