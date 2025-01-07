630d7962b480b4e239a0b034b8821864ea6613ed
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-01-07 02:57:31 -0500

    code cleanup

---
+++ lib/recoverbull_dart.dart
@@ -0,0 +1,9 @@
+export 'src/utils.dart';
+export 'src/models/backup_data.dart';
+export 'src/models/encryption_data.dart';
+export 'src/models/key_info.dart';
+export 'src/services/backup.dart';
+export 'src/services/encryption.dart';
+export 'src/services/key_management.dart';
+export 'src/storage/google_drive.dart';
+export 'src/storage/storage_interface.dart';
---
+++ lib/src/storage/i_cloud.dart
@@ -0,0 +1,23 @@
+import 'package:googleapis/drive/v3.dart';
+import 'package:recoverbull_dart/src/storage/storage_interface.dart';
+
+class ICloudStorage extends BackupStorage {
+  ICloudStorage(super.recoveryPath);
+  @override
+  Future<List<File>> readAllMetaDataFiles() {
+    // TODO: implement readAllMetaDataFiles
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<int>> readMetaDataContent(File file) {
+    // TODO: implement readMetaDataContent
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> writeMetaData(List<int> data, String fileName) {
+    // TODO: implement writeMetaData
+    throw UnimplementedError();
+  }
+}
