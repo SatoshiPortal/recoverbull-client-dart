c49577fdf1c21c6fcded217dfc76c34e3b5520f2
StaxoLotl <BitcoinStaxoLotl@proton.me>
2025-01-15 16:34:20 -0500

    refactor: remove unused storage classes and related exports

--- lib/recoverbull_dart.dart
+++ lib/recoverbull_dart.dart
@@ -1,7 +1,4 @@
 export 'src/utils.dart';
 export 'src/models/backup_data.dart';
-export 'src/models/key_info.dart';
 export 'src/services/backup.dart';
 export 'src/services/key_management.dart';
-export 'src/storage/google_drive.dart';
-export 'src/storage/storage_interface.dart';
--- lib/src/storage/storage_interface.dart
+++
@@ -1,9 +0,0 @@
-import 'package:googleapis/drive/v3.dart';
-
-abstract class BackupStorage {
-  final String recoveryPath;
-  BackupStorage(this.recoveryPath);
-  Future<bool> writeMetaData(List<int> data, String fileName);
-  Future<List<int>> readMetaDataContent(File file);
-  Future<List<File>> readAllMetaDataFiles();
-}
