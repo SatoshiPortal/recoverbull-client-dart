1a90782035e61ae6c9f2339545ba939ea21104b3
ethicnology <ethicnology@pm.me>
2025-01-28 12:47:45 -0500

    test: BackupService

---
+++ test/backup_test.dart
@@ -0,0 +1,71 @@
+import 'dart:convert';
+
+import 'package:hex/hex.dart';
+import 'package:recoverbull/recoverbull.dart';
+import 'package:recoverbull/src/services/encryption.dart';
+import 'package:test/test.dart';
+
+void main() {
+  final key = HEX.decode(
+      'fcb4a38e1d732dede321d13a6ffa024a38ecc4f40c88e9dcc3c9fe51fb942a6f');
+  final secret = utf8.encode('Hello, Encryption!');
+
+  final backupJson = BackupService.createBackup(
+    secret: secret,
+    backupKey: key,
+  );
+
+  final backup = Backup.fromString(backupJson);
+  final nonce = HEX.decode(backup.nonce);
+  final ciphertext = HEX.decode(backup.ciphertext);
+  final mac = HEX.decode(backup.mac);
+
+  group('EncryptionService', () {
+    test('create backup', () {
+      expect(backupJson, isNotEmpty);
+    });
+
+    test('test MAC', () {
+      final computedMac = EncryptionService.computeMac(
+        ciphertext: ciphertext,
+        nonce: nonce,
+        key: key,
+      );
+
+      expect(mac, computedMac);
+    });
+
+    test('restore', () {
+      final restoredSecret = BackupService.restoreBackup(
+        backup: backupJson,
+        backupKey: key,
+      );
+
+      expect(utf8.encode(restoredSecret), secret);
+    });
+
+    // test('restore', () {
+    //   final secret = utf8.encode('Super Secret!');
+    //   final mnemonic =
+    //       'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about';
+    //   final derivationPath = "m/1999'/0'"; // this is an example path
+
+    //   final backupJson = BackupService.createBackupWithBIP85(
+    //     secret: secret,
+    //     mnemonic: mnemonic,
+    //     derivationPath: derivationPath,
+    //     language: 'english', // optional
+    //     network: 'mainnet', // optional
+    //   );
+
+    //   final restoredSecret = BackupService.restoreBackupFromBip85(
+    //     backup: backupJson,
+    //     mnemonic: mnemonic,
+    //     derivationPath: derivationPath,
+    //     language: 'english', // optional
+    //     network: 'mainnet', // optional
+    //   );
+    //   assert(utf8.encode(restoredSecret) == secret);
+    // });
+  });
+}
