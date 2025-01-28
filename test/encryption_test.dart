9300bc6eb43fb0c0aad6cfe0b57c1c6f67263045
ethicnology <ethicnology@pm.me>
2025-01-28 11:00:49 -0500

    test: EncryptionService

---
+++ test/encryption_test.dart
@@ -0,0 +1,56 @@
+import 'dart:convert';
+
+import 'package:hex/hex.dart';
+import 'package:recoverbull/src/models/exceptions.dart';
+import 'package:recoverbull/src/services/encryption.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('EncryptionService', () {
+    final key = HEX.decode(
+        'fcb4a38e1d732dede321d13a6ffa024a38ecc4f40c88e9dcc3c9fe51fb942a6f');
+    final plaintext = utf8.encode('Hello, Encryption!');
+
+    final encryption = EncryptionService.encrypt(
+      key: key,
+      plaintext: plaintext,
+    );
+
+    test('encrypt and decrypt', () {
+      expect(encryption.ciphertext.isNotEmpty, true);
+      expect(encryption.nonce.isNotEmpty, true);
+      expect(encryption.mac.isNotEmpty, true);
+
+      final ciphertext = EncryptionService.decrypt(
+        key: key,
+        ciphertext: encryption.ciphertext,
+        nonce: encryption.nonce,
+      );
+
+      expect(utf8.decode(ciphertext), equals(utf8.decode(plaintext)));
+    });
+
+    test('decrypt with MAC', () {
+      final ciphertext = EncryptionService.decrypt(
+        key: key,
+        ciphertext: encryption.ciphertext,
+        nonce: encryption.nonce,
+        mac: encryption.mac,
+      );
+
+      expect(utf8.decode(ciphertext), equals(utf8.decode(plaintext)));
+    });
+
+    test('decrypt with INVALID MAC ', () {
+      expect(
+        () => EncryptionService.decrypt(
+          key: key,
+          ciphertext: encryption.ciphertext,
+          nonce: encryption.nonce,
+          mac: [0],
+        ),
+        throwsA(isA<EncryptionException>()),
+      );
+    });
+  });
+}
